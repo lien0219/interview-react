6eecc9d62e77f5b3cc264dc233165f4e8c1cba83
lien0219 <1824851183@qq.com>
2025-10-10 16:18:55 +0800

    feat: url

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,32 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Home from '../pages/home';
-import About from '../pages/about';
-import Contact from '../pages/contact';
-import App from '../App';
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home";
+import About from "../pages/about";
+import Contact from "../pages/contact";
+import App from "../App";
+import { isDev } from "../utils/env";
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App/>,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/about',
-        element: <About />,
-      },
-      {
-        path: '/contact',
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+const basename = isDev ? "" : "/interview-react/";
+
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ],
+  { basename }
+);
