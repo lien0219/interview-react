b5ce81346448031bd6b306898fa506fa58dfa52a
lien0219 <1824851183@qq.com>
2025-10-10 16:00:57 +0800

    feat: 路径

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8080
-  },
-  // base: '/interview-react/'
+export default defineConfig(({ mode }) => {
+  const basePath = mode === 'development' ? '/' : '/interview-react/'
+  return {
+    plugins: [react()],
+    server: {
+      port: 8080
+    },
+    base: basePath
+  }
 })
